14fd1d3dc1d6aeedd49f2a2c37a55b33ff8fc6de
Dane B. <dbake106@students.kennesaw.edu>
2025-11-29 14:32:00 -0500

    Signup
    I know my job is to set up for management and stuff, but I just wanted to get this done to show that I'm active and I'm working on the application.

--- Assets/Scripts/KioskSignup.cs
+++ Assets/Scripts/KioskSignup.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -5,14 +6,47 @@ using UnityEngine.UI;
 
 public class KioskSignup : MonoBehaviour
 {
-    public TMP_InputField emailphone;
-    public TMP_InputField emailphoneConfirm;
-    public TMP_InputField password;
-    public TMP_InputField passwordConfirm;
-    
+    public TMP_InputField emailphone, emailphoneConfirm;
+    public TextMeshProUGUI invalidatorText;
+    public TMP_InputField password, passwordConfirm;
+
     public void Signup()
     {
-        //Signup logic here
+        //Checks if all the boxes are filled
+        if (String.IsNullOrEmpty(emailphone.text) || String.IsNullOrEmpty(emailphoneConfirm.text) ||
+            String.IsNullOrEmpty(password.text) || String.IsNullOrEmpty(passwordConfirm.text))
+        {
+            invalidatorText.text = "! Please make sure to fill in all of the boxes.";
+        }
+        else
+        {
+            //Checks if the emails and passwords match to set for the Login Screen
+            bool emailphoneMatch = emailphone.text == emailphoneConfirm.text;
+            bool passwordMatch = password.text == passwordConfirm.text;
+
+            if (!emailphoneMatch || !passwordMatch)
+            {
+                if (!emailphoneMatch && !passwordMatch)
+                {
+                    invalidatorText.text = "! Emails and passwords do not match!";
+                }
+                else if (!emailphoneMatch)
+                {
+                    invalidatorText.text = "! Emails do not match!";
+                }
+                else if (!passwordMatch)
+                {
+                    invalidatorText.text = "! Passwords do not match!";
+                }
+            }
+            else
+            {
+                //Sets client's username and password to what was entered in the boxes and sends client back to the login screen if all checks succeed
+                StaticValues.User = emailphone.text;
+                StaticValues.Pass = password.text;
+                ToLogin();
+            }
+        } 
     }
     public void ToLogin()
     {
